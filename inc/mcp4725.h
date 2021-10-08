df1bb226e2d36a1c8f1df74675ca1a70395d98ae
Adam Siemasz <asiemasz1@gmail.com>
2021-10-08 18:32:34 +0200

    Correct some functions prototypes

--- inc/mcp4725.h
+++ inc/mcp4725.h
@@ -9,7 +9,7 @@
 #include "i2c.h"
 
 typedef enum {
-    MCP54725_ADDR_0 = 0x60,
+    MCP4725_ADDR_0 = 0x60,
     MCP4725_ADDR_1 =  0x61
 } MCP4725_ADDRESS;
 
@@ -17,7 +17,7 @@ typedef enum {
     MCP4725_FAST_MODE =       0x00,
     MCP4725_DAC_MODE =        0x40,
     MCP4725_DAC_EEPROM_MODE = 0x60
-} MCP4725_COMMAND;
+} MCP4725_COMMAND_MODE;
 
 typedef enum {
     MCP4725_POWER_DOWN_OFF =    0x00,
@@ -41,7 +41,18 @@ typedef struct MCP4725 {
 	MCP4725_ADDRESS address;
 	float refVoltage;
 	uint16_t bitsPerVolt;
+  MCP4725_POWER_DOWN_MODE powerDownMode;
 } MCP4725;
 
+MCP4725 MCP4725_init(I2C_TypeDef * i2c_dev, MCP4725_ADDRESS addr, float vRef, MCP4725_POWER_DOWN_MODE mode);
+void MC4725_setValue(MCP4725* dev, uint16_t value, MCP4725_COMMAND_MODE mode);
+void MCP4725_setVoltage(MCP4725* dev, float voltage, MCP4725_COMMAND_MODE mode);
+uint16_t MCP4725_getValue(MCP4725* dev);
+float MCP4725_getVoltage(MCP4725* dev);
+float MCP4725_getEEPROMVoltage(MCP4725 *dev);
+uint8_t MCP4725_getPowerType(MCP4725 *dev);
+uint8_t MCP4725_getEEPROMPowerType(MCP4725 *dev);
+void	MCP4725_reset(MCP4725* dev);
+void	MCP4725_wakeUp(MCP4725* dev);
 
 #endif
