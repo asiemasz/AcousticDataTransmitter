b2ab22f3326cac760386e61b7b00579751518343
Adam Siemasz <asiemasz1@gmail.com>
2021-10-16 13:57:35 +0200

    add initialization of uart

---
+++ src/uart.c
@@ -0,0 +1,66 @@
+#include "../inc/uart.h"
+
+void uart_init(UART_initStruct *init)
+{
+    struct uart_pins pins;
+    //enable appropriate uart clock and select pins to configure
+    if (init->uart == USART1)
+    {
+        RCC->APB2ENR |= RCC_APB2ENR_USART1EN;
+        pins = uart1_pins;
+    }
+    else if (init->uart == USART2)
+    {
+        RCC->APB1ENR |= RCC_APB1ENR_USART2EN;
+        pins = uart2_pins;
+
+    }
+    else if (init->uart == USART6)
+    {
+        RCC->APB2ENR |= RCC_APB2ENR_USART6EN;
+        pins = uart6_pins;
+    }
+    else
+    {
+        assert(0);
+    }
+
+    //configure pins
+    gpio_init(pins.uart_tx_port);
+	  gpio_set_pin_mode(pins.uart_tx_port, pins.uart_tx_pin, GPIO_MODE_ALTERNATE_FUNCTION);
+    gpio_set_pin_mode(pins.uart_rx_port, pins.uart_rx_pin, GPIO_MODE_ALTERNATE_FUNCTION);
+    gpio_set_alternate_function(pins.uart_tx_port, pins.uart_tx_pin, pins.uart_tx_af);
+    gpio_set_alternate_function(pins.uart_rx_port, pins.uart_rx_pin, pins.uart_rx_af);
+
+    //clear USART CR1 register
+    init->uart->CR1 = 0x00;
+    //enable usart
+    init->uart->CR1 |= USART_CR1_UE;
+    //select word length
+    init->uart->CR1 |= (uint32_t) init->wordLength << USART_CR1_M_Pos;
+    //select parity control
+    init->uart->CR1 |= (uint32_t) init->parityControl << USART_CR1_PCE_Pos;
+    if(init->parityControl == UART_PARITY_CONTROL_ENABLED)
+        init->uart->CR1 |= (uint32_t) init->paritySelection << USART_CR1_PS_Pos;
+    //select oversampling mode
+    init->uart->CR1 |= (uint32_t) init->oversampling << USART_CR1_OVER8_Pos;
+    //set transmitter/receiver mode
+    init->uart->CR1 |= (uint32_t) init->mode << USART_CR1_RE_Pos;
+    volatile uint32_t pclk;
+    //set the baudrate
+    if(init->uart == USART2) {
+        pclk = 42000000;
+    }
+    else {
+        pclk = 84000000;
+    }
+		//calculate appropriate mantisa and fraction of baudRate according to selected oversampling mode (calculated using method from HAL USART library)
+    uint32_t div = (uint32_t)(pclk*25U/(2U*(2U-(uint8_t)init->oversampling)*init->baudRate));
+    uint32_t mant = div/100U;
+    uint32_t fraq = ((((div - (mant * 100U)) * 8U*(2U-(uint8_t)init->oversampling)) + 50U) / 100U);
+		//set baudrate to its register
+    if(init->oversampling)
+        init->uart->BRR = ((mant << 4U) + ((fraq & 0xF8U) << 1U) + (fraq & 0x07U));
+    else
+        init->uart->BRR = ((mant << 4U) + (fraq & 0xF0U) + (fraq & 0x0FU));
+}
