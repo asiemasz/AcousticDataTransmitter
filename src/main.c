c09edf5e2922a4f1414feafa8c0aaa6b83bfb466
Adam Siemasz <asiemasz1@gmail.com>
2021-10-31 14:45:13 +0100

    Sample sinus generation with SPI and DAC

--- src/main.c
+++ src/main.c
@@ -8,20 +8,42 @@
 #include "MCP4822.h"
 
 SPI_initStruct spi1;
+TIMER_initStruct tim2;
 
-void MCP4822_write(const uint8_t* data, const uint16_t len) {
+void MCP4822_write(const uint8_t *data, const uint16_t len)
+{
 	spi_write(&spi1, data, len);
 }
 
-void MCP4822_CSen(const uint32_t* GPIO_port,const uint8_t GPIO_pin) {
+void MCP4822_CSen(const uint32_t *GPIO_port, const uint8_t GPIO_pin)
+{
 	gpio_resetPin(GPIO_port, GPIO_pin);
 }
 
-void MCP4822_CSdis(const uint32_t* GPIO_port,const uint8_t GPIO_pin) {
+void MCP4822_CSdis(const uint32_t *GPIO_port, const uint8_t GPIO_pin)
+{
 	gpio_setPin(GPIO_port, GPIO_pin);
 }
 
-int main() {
+MCP4822_device MCP4822 = {
+	.SS_pin = PIN3,
+	.SS_Port = GPIOA,
+	.writeFunction = MCP4822_write,
+	.CSenableFunction = MCP4822_CSen,
+	.CSdisableFunction = MCP4822_CSdis};
+
+MCP4822_OUTPUT_CONFIG cfg = {
+	.gain = MCP4822_OUTPUT_GAIN_x1,
+	.output = MCP4822_DAC_A,
+	.powerDown = MCP4822_OUTPUT_POWERDOWN_CONTROL_BIT,
+};
+
+const uint16_t sine[20] = {2048, 2712, 3305, 3762, 4032, 4088, 3923, 3554, 3022, 2385,
+						   1710, 1073, 541, 172, 7, 63, 333, 790, 1383, 2047};
+volatile uint8_t i = 0;
+
+int main()
+{
 	gpio_init(GPIOA);
 	gpio_init(GPIOC);
 
@@ -50,30 +72,29 @@ int main() {
 	spi1.dataLength = SPI_DATA_FRAME_LENGTH_8_BITS;
 	spi1.spi = SPI1;
 
+	tim2.tim = TIM2;
+	tim2.direction = TIMER_COUNTER_DIRECTION_DOWN;
+	tim2.prescaler = 1;
+	tim2.autoReload = 233;
+
 	spi_init(&spi1);
+	timer_init(&tim2);
+	timer_enableIT(&tim2, TIMER_IT_UPDATE_EVENT);
 
-	MCP4822_device MCP4822 = {
-		.SS_pin = PIN3,
-		.SS_Port = GPIOA,
-		.writeFunction = MCP4822_write,
-		.CSenableFunction = MCP4822_CSen,
-		.CSdisableFunction = MCP4822_CSdis
-	};
+	NVIC_EnableIRQ(TIM2_IRQn);
 
-	MCP4822_OUTPUT_CONFIG cfg = {
-		.gain = MCP4822_OUTPUT_GAIN_x2,
-		.output = MCP4822_DAC_A,
-		.powerDown = MCP4822_OUTPUT_POWERDOWN_CONTROL_BIT,
-	};
+	timer_start(&tim2);
 
-	uint16_t val[4] = {0x0, 0xf, 0xff, 0xfff};
-	uint8_t i = 0;
-
-	while(1) {
-		if(i > 4)
-			i = 0;
-		MCP4822_setValue(&MCP4822, val[i++], &cfg);
-		gpio_togglePin(GPIOC, PIN3);
-	}		
+	while (1)
+	{
+	}
 }
- 
+
+void TIM2_IRQHandler()
+{
+	timer_clearITflag(&tim2);
+	MCP4822_setValue(&MCP4822, sine[i++], &cfg);
+	if(i == 20) {
+		i = 0;
+	}
+}
