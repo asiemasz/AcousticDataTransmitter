e85205f8e069bd5cd3f312774302f5070ff2f5fc
Adam Siemasz <asiemasz1@gmail.com>
2021-11-07 20:06:58 +0100

    Sample frequency on time switching

--- main.c
+++ main.c
@@ -7,8 +7,41 @@
 #include "spi.h"
 #include "MCP4822.h"
 
-SPI_initStruct spi1;
-TIMER_initStruct tim2;
+#define SAMPLES 20
+#define SYMBOLS 18
+
+const SPI_pinout spi1_pins = {
+	.af = GPIO_AF5,
+	.CLK_Pin = PIN3,
+	.CLK_Port = GPIOB,
+	.MISO_Pin = PIN6,
+	.MISO_Port = GPIOA,
+	.MOSI_Pin = PIN7,
+	.MOSI_Port = GPIOA,
+};
+
+const SPI_initStruct spi1 = {
+	.clkPrescaller = SPI_MASTER_CLOCK_PRESCALLER_8,
+	.clockMode = SPI_CLOCK_MODE_POLARITY_0_PHASE_0,
+	.mode = SPI_MODE_MASTER,
+	.slaveSelectMode = SPI_SOFTWARE_NSS_MANAGEMENT,
+	.pinout = spi1_pins,
+	.dataLength = SPI_DATA_FRAME_LENGTH_8_BITS,
+	.spi = SPI1,
+};
+
+const TIMER_initStruct tim2 = {
+	.tim = TIM2,
+	.direction = TIMER_COUNTER_DIRECTION_DOWN,
+	.prescaler = 1,
+	.autoReload = 1750,
+};						  // DAC handling
+TIMER_initStruct tim3 = { // changing frequency (TIMER2 reload value register)
+	.direction = TIMER_COUNTER_DIRECTION_DOWN,
+	.prescaler = 1000,
+	.autoReload = 8400,
+	.tim = TIM3
+};
 
 void MCP4822_write(const uint8_t *data, const uint16_t len)
 {
@@ -38,16 +71,24 @@ MCP4822_OUTPUT_CONFIG cfg = {
 	.powerDown = MCP4822_OUTPUT_POWERDOWN_CONTROL_BIT,
 };
 
-const uint16_t sine[20] = {0x7fd,0xa75,0xcaf,0xe73,0xf96,0xffa,0xf96,0xe73,0xcaf,0xa75,0x7fd,0x585,0x34b,0x187,0x64,0x0,0x64,0x187,0x34b,0x585};
-//const uint16_t sine[10] = {0x7fd,0xcaf,0xf96,0xf96,0xcaf,0x7fd,0x34b,0x64,0x64,0x34b};
-//const uint16_t sine[15] = {0x7fd,0xb3d,0xded,0xf96,0xfef,0xee8,0xcaf,0x9a6,0x654,0x34b, 0x112,0xb,0x64,0x20d,0x4bd,0x7fd};
-//const uint16_t sine[18] = {0x7d0,0xa7c,0xcd6,0xe94,0xf82,0xf82,0xe94,0xcd6,0xa7c,0x7d0,0x524,0x2ca,0x10c,0x1e,0x1e,0x10c,0x2ca,0x524}; 
+const uint16_t sine[SAMPLES] = {0x7fd, 0xa75, 0xcaf, 0xe73, 0xf96, 0xffa, 0xf96, 0xe73, 0xcaf, 0xa75, 0x7fd, 0x585, 0x34b, 0x187, 0x64, 0x0, 0x64, 0x187, 0x34b, 0x585};
+// const uint16_t sine[10] = {0x7fd,0xcaf,0xf96,0xf96,0xcaf,0x7fd,0x34b,0x64,0x64,0x34b};
+// const uint16_t sine[15] = {0x7fd,0xb3d,0xded,0xf96,0xfef,0xee8,0xcaf,0x9a6,0x654,0x34b, 0x112,0xb,0x64,0x20d,0x4bd,0x7fd};
+// const uint16_t sine[18] = {0x7d0,0xa7c,0xcd6,0xe94,0xf82,0xf82,0xe94,0xcd6,0xa7c,0x7d0,0x524,0x2ca,0x10c,0x1e,0x1e,0x10c,0x2ca,0x524};
 
 volatile uint8_t i = 0;
+volatile uint8_t j = 0;
+uint16_t reload[SYMBOLS];
+
 volatile uint8_t dataReady = 0;
 
 int main()
 {
+	//fill reload array
+	for (uint8_t i = 0; i < SYMBOLS; i++) {
+		reload[i] = 84000000/SAMPLES/(17000 + i*3000/SYMBOLS);
+	}
+
 	gpio_init(GPIOA);
 	gpio_init(GPIOC);
 
@@ -57,42 +98,24 @@ int main()
 	};
 	gpio_setPinConfiguration(GPIOC, PIN3, &pin3);
 
-	SPI_pinout spi1_pins = {
-		.af = GPIO_AF5,
-		.CLK_Pin = PIN3,
-		.CLK_Port = GPIOB,
-		.MISO_Pin = PIN6,
-		.MISO_Port = GPIOA,
-		.MOSI_Pin = PIN7,
-		.MOSI_Port = GPIOA,
-	};
-
-	spi1.clkPrescaller = SPI_MASTER_CLOCK_PRESCALLER_8;
-	spi1.clockMode = SPI_CLOCK_MODE_POLARITY_0_PHASE_0;
-	spi1.mode = SPI_MODE_MASTER;
-	spi1.slaveSelectMode = SPI_SOFTWARE_NSS_MANAGEMENT;
-	spi1.pinout = spi1_pins;
-	spi1.dataLength = SPI_DATA_FRAME_LENGTH_8_BITS;
-	spi1.spi = SPI1;
-
-	tim2.tim = TIM2;
-	tim2.direction = TIMER_COUNTER_DIRECTION_DOWN;
-	tim2.prescaler = 1;
-	tim2.autoReload = 210;
-
 	spi_init(&spi1);
 	timer_init(&tim2);
+	timer_init(&tim3);
 	timer_enableIT(&tim2, TIMER_IT_UPDATE_EVENT);
+	timer_enableIT(&tim3, TIMER_IT_UPDATE_EVENT);
+
 
 	NVIC_EnableIRQ(TIM2_IRQn);
+	NVIC_EnableIRQ(TIM3_IRQn);
 
 	timer_start(&tim2);
-	//MCP4822_setVoltage(&MCP4822, 1.3f, &cfg);
+	timer_start(&tim3);
 
 	while (1)
 	{
-		if(dataReady) {
-			MCP4822_setValue(&MCP4822, sine[i], &cfg); 
+		if (dataReady)
+		{
+			MCP4822_setValue(&MCP4822, sine[i], &cfg);
 			dataReady = 0;
 		}
 	}
@@ -100,10 +123,20 @@ int main()
 
 void TIM2_IRQHandler()
 {
-	TIM2->SR = 0;	//For faster performance callbacks' not used.
+	timer_clearITflag(&tim2);
 	dataReady = 1;
-	if(++i == 20) {
+	if (++i == SAMPLES)
+	{
 		i = 0;
-		TIM2->ARR = 221;
+	}
+}
+
+void TIM3_IRQHandler()
+{
+	timer_clearITflag(&tim3);
+	timer_setReloadVal(&tim2, reload[j]);
+	if (++j == SYMBOLS)
+	{
+		j = 0;
 	}
 }
