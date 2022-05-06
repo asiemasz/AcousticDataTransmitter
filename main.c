37790042e514b8f1a3554f77a55ccf1dc82c9657
asiemasz <asiemasz1@gmail.com>
2022-05-06 21:24:44 +0200

    refactor transmitter

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #include "adc.h"
 #include "arm_math.h"
 #include "gpio.h"
+#include "random_generator.h"
 #include "spi.h"
 #include "stm32f401xe.h"
 #include "timer.h"
@@ -11,19 +12,16 @@
 
 #define FS 48000
 #define FSystem 84000000
-#define FC 16000
-#define ROLLOVER_FACTOR 0.25
-#define FSPAN 4
-#define FB 888.88
-#define SPB 54
-#define N_BYTES 1
-#define PREFIX_LENGTH 200
-
-#define DATA_LENGTH (SPB * (N_BYTES)*8 + PREFIX_LENGTH)
+#define FC 8000
+#define ROLLOVER_FACTOR 0.5
+#define FSPAN 2
+#define SPB 24
+#define DATA_FRAME_LENGTH 8
+#define PREAMBLE_LENGTH 5
 
 extern uint32_t SystemCoreClock;
 
-const SPI_pinout spi1_pins = {
+const static SPI_pinout spi1_pins = {
     .af = GPIO_AF5,
     .CLK_Pin = PIN3,
     .CLK_Port = GPIOB,
@@ -33,7 +31,7 @@ const SPI_pinout spi1_pins = {
     .MOSI_Port = GPIOA,
 };
 
-const SPI_initStruct spi1 = {
+const static SPI_initStruct spi1 = {
     .clkPrescaller = SPI_MASTER_CLOCK_PRESCALLER_8,
     .clockMode = SPI_CLOCK_MODE_POLARITY_0_PHASE_0,
     .mode = SPI_MODE_MASTER,
@@ -43,7 +41,7 @@ const SPI_initStruct spi1 = {
     .spi = SPI1,
 };
 
-const TIMER_initStruct tim2 = {
+const static TIMER_initStruct tim2 = {
     // sampling frequency
     .tim = TIM2,
     .direction = TIMER_COUNTER_DIRECTION_DOWN,
@@ -62,7 +60,14 @@ void MCP4822_CSen(const uint32_t *GPIO_port, const uint8_t GPIO_pin) {
 void MCP4822_CSdis(const uint32_t *GPIO_port, const uint8_t GPIO_pin) {
   gpio_setPin(GPIO_port, GPIO_pin);
 }
-static UART_initStruct uart2;
+const static UART_initStruct uart2 = {.baudRate = 115200,
+                                      .mode = UART_TRANSMITTER_ONLY,
+                                      .oversampling = UART_OVERSAMPLING_BY_16,
+                                      .parityControl =
+                                          UART_PARITY_CONTROL_DISABLED,
+                                      .stopBits = UART_STOP_BITS_1,
+                                      .wordLength = UART_WORD_LENGTH_8,
+                                      .uart = USART2};
 
 MCP4822_device MCP4822 = {.SS_pin = PIN3,
                           .SS_Port = GPIOC,
@@ -76,28 +81,35 @@ MCP4822_OUTPUT_CONFIG cfg = {
     .powerDown = MCP4822_OUTPUT_POWERDOWN_CONTROL_BIT,
 };
 
+static const int8_t preamble[PREAMBLE_LENGTH] = {1, 1, 1, -1, 1};
+
+static BPSK_parameters params = {.Fb = FS / SPB,
+                                 .Fc = FC,
+                                 .frameLength = DATA_FRAME_LENGTH,
+                                 .FSpan = FSPAN,
+                                 .preambleCode = preamble,
+                                 .preambleCodeLength = PREAMBLE_LENGTH,
+                                 .samplesPerBit = SPB};
+
+const float32_t FN = (float32_t)FC / FS;
+
 volatile uint8_t dataReady = 0;
-volatile float32_t y;
 volatile uint16_t val;
 
-volatile uint64_t i = 0;
+volatile uint32_t i = 0;
 volatile uint32_t j = 0;
 
-uint8_t data[N_BYTES] = {25}; //, 161, 149};
-
-float32_t txSignal[DATA_LENGTH];
 float32_t coeffs[FSPAN * SPB + 1];
+static float32_t
+    modulated_signal[SPB * (DATA_FRAME_LENGTH + PREAMBLE_LENGTH) * 10] = {0};
 
 int main() {
-  SRRC_getFIRCoeffs(FSPAN, SPB, ROLLOVER_FACTOR, coeffs, FSPAN * SPB + 1);
+  initRandomGenerator(2137);
 
-  BPSK_parameters params = {.Fb = FB,
-                            .Fs = FS,
-                            .Fc = FC,
-                            .FSpan = FSPAN,
-                            .prefixLength = PREFIX_LENGTH,
-                            .firCoeffs = coeffs,
-                            .firCoeffsLength = FSPAN * SPB + 1};
+  SRRC_getFIRCoeffs(FSPAN, SPB, ROLLOVER_FACTOR, coeffs, FSPAN * SPB + 1, 3.7f);
+
+  params.matchedFilterCoeffs = coeffs;
+  params.matchedFilterCoeffsLength = FSPAN * SPB + 1;
 
   gpio_init(GPIOA);
   gpio_init(GPIOC);
@@ -108,14 +120,6 @@ int main() {
   };
   gpio_setPinConfiguration(GPIOC, PIN3, &pin3);
 
-  uart2.baudRate = 115200;
-  uart2.mode = UART_TRANSMITTER_ONLY;
-  uart2.oversampling = UART_OVERSAMPLING_BY_16;
-  uart2.parityControl = UART_PARITY_CONTROL_DISABLED;
-  uart2.stopBits = UART_STOP_BITS_1;
-  uart2.wordLength = UART_WORD_LENGTH_8;
-  uart2.uart = USART2;
-
   uart_init(&uart2);
   spi_init(&spi1);
   timer_init(&tim2);
@@ -124,39 +128,37 @@ int main() {
   NVIC_EnableIRQ(TIM2_IRQn);
   NVIC_EnableIRQ(TIM3_IRQn);
 
-  BPSK_getOutputSignal(&params, data, N_BYTES, txSignal, DATA_LENGTH);
+  char buf[20];
+
+  uint8_t random_byte[10] = {203U}; // generateRandomFromRange(0, 255);
+  arm_fill_q7(203, random_byte, 10);
+  BPSK_getOutputSignalWithPreamble(&params, random_byte, 10, modulated_signal,
+                                   10 * SPB *
+                                       (DATA_FRAME_LENGTH + PREAMBLE_LENGTH));
 
   timer_start(&tim2);
-
-  float32_t fn = (float32_t)FC / FS;
-
   while (1) {
     if (dataReady) {
-      y = (txSignal[j - 1] * arm_sin_f32(fn * (i - 1) * 2 * PI) + 1.0f) / 2.0f;
+      float32_t y = (modulated_signal[j] *
+                     arm_cos_f32(FN * i * 2.0f * PI)); // + 1.0f) / 2.0f;
       val = y * 4000.0f;
       MCP4822_setValue(&MCP4822, val, &cfg);
-      if (j == DATA_LENGTH) {
-        j = 0;
-      }
-      if (i == 0xFFFFFFFFFFFFFFFF) {
-        i = 0;
-      }
       dataReady = 0;
     }
   }
 }
 
 void TIM2_IRQHandler() {
+  timer_clearITflag(&tim2);
   if (dataReady) {
     char buf[20];
     sprintf(buf, "Overrun");
     uart_sendString(&uart2, buf);
     Default_Handler();
   }
-  timer_clearITflag(&tim2);
 
-  ++i;
-  ++j;
+  i = (i + 1) % (FS / FC);
+  j = (j + 1) % (10 * SPB * (DATA_FRAME_LENGTH + PREAMBLE_LENGTH));
 
   dataReady = 1;
 }
